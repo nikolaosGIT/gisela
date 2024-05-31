6507d0455191ebd95e8e8631da4ec4b14c552c22
Nikolaos Wassiliadis <nikolaos.wassiliadis@gmail.com>
2024-05-31 22:18:18 +0200

    Added pump crontab routine

--- activate.py
+++ activate.py
@@ -1,11 +1,27 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(23, GPIO.OUT)
-# loop through 50 times, on/off for 1 second
-for i in range(10):
-    GPIO.output(7,True)
-    time.sleep(1)
-    GPIO.output(7,False)
-    time.sleep(1)
-GPIO.cleanup()
+#!/usr/bin/python3
+
+def activate(seconds):
+	import RPi.GPIO as GPIO
+	import time
+	from datetime import datetime
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(23, GPIO.OUT) # Positive
+	GPIO.setup(24, GPIO.OUT) # Negative
+	# Pump on
+	now = datetime.now()
+	current = now.strftime("%H:%M:%S %Y:%m:%d")
+	print("System online at", current)
+	#GPIO.output(23,True)
+	#GPIO.output(24,True)
+	# Keep on for 3 seconds
+	time.sleep(seconds)
+	# Pump off
+	now = datetime.now()
+	current = now:strftime("%H:%M:%S %Y:%m:%d")
+	print("System offline at", current)
+	#GPIO.output(23,False)
+	#GPIO.output(24,False)
+	GPIO.cleanup()
+
+if __name__ == '__main__':
+	activate(1)
